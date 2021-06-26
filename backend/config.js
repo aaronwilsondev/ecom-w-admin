08e0308425e4d2859575db39c0d74c32365b930d
Aaron Wilson <aaronwilson1234@outlook.ie>
2021-06-26 14:12:23 +0100

    testing

--- backend/config.js
+++ backend/config.js
@@ -14,12 +14,3 @@ export default {
 
 }
 
-
-// curl -s --user 'api:b2ad9800b79308f82316db1ffb32ed69-24e2ac64-245d2b15' \
-// https://api.eu.mailgun.net/v3/emsiart.com/messages \
-// -F from='Excited User <mailgun@emsiart.com>' \
-// -F to=aaronwilson1234@outlook.ie \
-// -F subject='Hello' \
-// -F text='Testing some Mailgun awesomeness!'
-
-/*mailgun testdata */
